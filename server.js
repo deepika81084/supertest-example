7ed0cfd20aa8de5ad1f3be2a7e64c8d757fd8a78
Deepika Chhabra <deepikachhabra@Deepikas-MacBook-Pro.local>
2020-08-21 12:17:09 +0530

    supertesAPI practice

--- server.js
+++ server.js
@@ -19,60 +19,60 @@ app.use(expressValidator({
 }));
 
 // Just to test our server is working
-app.get('/api', function(req, res) {
-  res.send({
-    version: '1.0.0'
-  });
-});
+//app.get('/api', function(req, res) {
+//  res.send({
+//    version: '1.0.0'
+//  });
+//});
 
 // Auth routes
-authRouter.post('/register', function(req, res) {
-  req.checkBody({
-    name: {
-      isAlpha: true,
-      isLength: {
-        options: [{ min: 2, max: 50 }],
-        errorMessage: 'Name must be between 2 and 50 characters.'
-      },
-      errorMessage: 'Name must have only alphabetical characters.'
-    }
-  });
+//authRouter.post('/register', function(req, res) {
+//  req.checkBody({
+//    name: {
+//      isAlpha: true,
+//      isLength: {
+//        options: [{ min: 2, max: 50 }],
+//        errorMessage: 'Name must be between 2 and 50 characters.'
+//      },
+//      errorMessage: 'Name must have only alphabetical characters.'
+//    }
+//  });
 
-  var errors = req.validationErrors();
+//  var errors = req.validationErrors();
+//
+//  if (errors) {
+//    return res.status(400).json({
+//      errors: errors
+//    });
+//  }
+//
+//  var userIndex = users.push(req.body) - 1;
+//
+//  res.json(users[userIndex]);
+//});
+//
+//authRouter.post('/login', function(req, res) {
+//  req.checkBody({
+//    userID: {
+//      isNumeric: true,
+//      isExistingUser: {
+//        errorMessage: 'That user does not exist.'
+//      },
+//      errorMessage: 'Authentication requires a number.'
+//    }
+//  });
+//
+//  var errors = req.validationErrors();
+//
+//  if (errors) {
+//    return res.status(400).json({
+//      errors: errors
+//    });
+//  }
+//
+//  res.json(users[req.body.userID]);
+//});
 
-  if (errors) {
-    return res.status(400).json({
-      errors: errors
-    });
-  }
-
-  var userIndex = users.push(req.body) - 1;
-
-  res.json(users[userIndex]);
-});
-
-authRouter.post('/login', function(req, res) {
-  req.checkBody({
-    userID: {
-      isNumeric: true,
-      isExistingUser: {
-        errorMessage: 'That user does not exist.'
-      },
-      errorMessage: 'Authentication requires a number.'
-    }
-  });
-
-  var errors = req.validationErrors();
-
-  if (errors) {
-    return res.status(400).json({
-      errors: errors
-    });
-  }
-
-  res.json(users[req.body.userID]);
-});
-
-app.use('/api', authRouter);
+//app.use('/api.bart.gov/', authRouter);
 
 module.exports = app;
--- tests.spec.js
+++ tests.spec.js
@@ -1,121 +1,78 @@
-var app = require('./server');
+//var app = require('./server');
+var nock = require('nock');
 var chai = require('chai');
 var request = require('supertest');
+var bodyParser = require('body-parser')
 
 var expect = chai.expect;
 
+//var auth = {'Authorization': 'Token MW9S-E7SL-26DU-VV8V', 'Content-Type': 'application/json'};
+
 describe('API tests', function() {
-  it('should have return version number', function(done) {
-    request(app)
-      .get('/api')
+  it('should have return bart.gov api result', function(done) {
+    request('api.bart.gov')
+      //.get('/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V&json=y')
+   //   nock('api.bart.gov')
+      .get('/api/stn.aspx?cmd=stninfo&orig=24th&key=MW9S-E7SL-26DU-VV8V&json=y')
+      //.get('/api/sched.aspx?cmd=stnsched&orig=12th&key=MW9S-E7SL-26DU-VV8V&l=1&json=y')
       .end(function(err, res) {
-        expect(res.body.version).to.be.ok;
-        expect(res.statusCode).to.be.equal(200);
+      console.log("res.body");
+      console.log(res.body);
+      //console.log(res.body)
+      console.log("stringyfy json");
+      console.log(JSON.stringify(res.body));
+      var jsonResponse = res.body;
+      var info = jsonResponse['root'].stations.station;
+     // var info = jsonResponse['root'].stations.station.north_routes.route[0];
+     //var info;
+//     for(var i=0;i<10;i++)
+//     {
+//      info = jsonResponse['root'].station.item[i];
+//      console.log("extract info trains");
+//       console.log(info);
+//       info = 0;
+//     }
+
+   // info = jsonResponse['root'].station.item[0];
+    console.log("extract info trains");
+    console.log(info);
+    expect(res.statusCode).to.be.equal(200);
+    console.log(info.name);
+    //expect(info.name).to.be.equal('24th St. Mission');
+//
+
+
+      //var jsonRes = bodyParser.json();
+      //console.log("jsonRes");
+      //console.log(jsonRes);
+      //JSONParser parse = new JSONParser();
+
+      //JSONObject jObj = (JSONObject)parse.parse(jsonResponse);
+      //jsonResponse[stations[0]];
+        //expect(res.body.version).to.be.ok;
+       //expect(res.body.message).to.be.equal('Success');
         done();
       });
   });
 
-  describe('Registration Tests', function() {
-    describe('Successes', function() {
-      it('should return the user if the name is valid', function(done) {
-        request(app)
-        .post('/api/register')
-        .send({name: 'JoshMatz'})
-        .end(function(err, res) {
-          expect(res.body.name).to.be.equal('JoshMatz');
-          expect(res.statusCode).to.be.equal(200);
-          done();
-        });
-      });
-    });
+  it('should have return bart.gov api mock result', function(done) {
+        nock('http://api.bart.gov')
+        .get('/api/stn.aspx?cmd=stninfo&orig=24th&key=MW9S-E7SL-26DU-VV8V&json=y')
+        .reply(200, {
+                "status": 200,
+                "message": "This is a mocked bart api response"
+              });
 
-    describe('Errors', function() {
-      it('should return multiple errors if no name is supplied', function(done) {
-        request(app)
-        .post('/api/register')
-        .end(function(err, res) {
-          expect(res.body.errors).to.be.an('array');
-          expect(res.statusCode).to.be.equal(400);
-          done();
+            request('http://api.bart.gov')
+            .get('/api/stn.aspx?cmd=stninfo&orig=24th&key=MW9S-E7SL-26DU-VV8V&json=y')
+            .end(function (err, res) {
+            console.log(res.body.status);
+            console.log(res.body.message);
+            //assert that the mocked response is returned
+            expect(res.body.status).to.equal(200);
+            expect(res.body.message).to.equal("This is a mocked bart api response");
+           done();
+           });
         });
-      });
 
-      it('should return return a length error if a name is too short', function(done) {
-        request(app)
-        .post('/api/register')
-        .send({ name: 'J'})
-        .end(function(err, res) {
-          expect(res.body.errors).to.be.an('array');
-          expect(res.body.errors[0].msg).to.be.equal('Name must be between 2 and 50 characters.');
-          expect(res.statusCode).to.be.equal(400);
-          done();
-        });
-      });
-
-      it('should return a character error if a name includes special characters', function(done) {
-        request(app)
-        .post('/api/register')
-        .send({ name: 'J0$#'})
-        .end(function(err, res) {
-          expect(res.body.errors).to.be.an('array');
-          expect(res.body.errors[0].msg).to.be.equal('Name must have only alphabetical characters.');
-          expect(res.statusCode).to.be.equal(400);
-          done();
-        });
-      });
-    });
-  });
-
-  describe('Login Tests', function() {
-    describe('Successes', function() {
-      it('should return the user if valid', function(done) {
-        request(app)
-        .post('/api/login')
-        .send({userID: 0})
-        .end(function(err, res) {
-          expect(res.body.name).to.be.equal('JoshMatz');
-          expect(res.statusCode).to.be.equal(200);
-          done();
-        });
-      });
-    });
-
-    describe('Errors', function() {
-      it('should return a numeric error if no user is supplied', function(done) {
-        request(app)
-        .post('/api/login')
-        .send({})
-        .end(function(err, res) {
-          expect(res.body.errors).to.be.an('array');
-          expect(res.body.errors[0].msg).to.be.equal('Authentication requires a number.');
-          expect(res.statusCode).to.be.equal(400);
-          done();
-        });
-      });
-
-      it('should return a numeric error if a non-number user is supplied', function(done) {
-        request(app)
-        .post('/api/login')
-        .send({ userID: '@#$' })
-        .end(function(err, res) {
-          expect(res.body.errors).to.be.an('array');
-          expect(res.body.errors[0].msg).to.be.equal('Authentication requires a number.');
-          expect(res.statusCode).to.be.equal(400);
-          done();
-        });
-      });
-
-      it('should return an non-existing error if an valid ID but non-existing user is supplied', function(done) {
-        request(app)
-        .post('/api/login')
-        .send({ userID: 5 })
-        .end(function(err, res) {
-          expect(res.body.errors).to.be.an('array');
-          expect(res.body.errors[0].msg).to.be.equal('That user does not exist.');
-          expect(res.statusCode).to.be.equal(400);
-          done();
-        });
-      });
-    });
-  });
 });
